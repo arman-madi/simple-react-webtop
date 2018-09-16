b55174f5a355cfbe3d880f780cafd4a487f95899
Iman <iman.ison@gmail.com>
2018-09-16 22:06:56 +1000

    Drag start shortcuts

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import './Desktop.css';
 
 class Desktop extends Component {
+    
+    componentDidMount() {
+
+    }
+
+    onDragOverHandler(e) {
+        e.preventDefault()
+    }
+
+    onDropHandler(e) {
+        console.log("DROP: " + e)
+    }
+
     render() {
+        
+        let dragableChildren = React.Children.map(this.props.children, (child)=>{
+            if (['Shortcut', 'Window'].indexOf(child.type.name) === -1){
+                return child
+            }
+            return React.cloneElement(child, {
+                attributes:{
+                    draggable: true, 
+                    onDragStart: (e) => {
+                        console.log("DRAG START: " + e.target.id)
+                        //e.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
+                        // TODO: investigate what is the best data to transfer
+                        e.dataTransfer.setData('text/plain', e.target.id)
+                    }
+            }})
+        })
+
         return (
-            <div className="desktop">
-                {this.props.children}
+            <div className="desktop" 
+                onDragOver={this.onDragOverHandler}
+                onDrop={this.onDropHandler}
+            >
+                {dragableChildren}
             </div>
         );
     }
--- src/components/Shortcut.js
+++ src/components/Shortcut.js
@@ -4,7 +4,7 @@ import './Shortcut.css';
 class Shortcut extends Component {
     render() {
         return (
-            <div className='shortcut' title='Shortcut'/>
+            <div id={this.props.key?this.props.key:'shortcut'} className='shortcut' title='Shortcut' {...this.props.attributes}/>
         );
     }
 }
