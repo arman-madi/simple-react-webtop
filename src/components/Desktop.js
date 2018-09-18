767ea721a7343e48c9e9e4f30dd3db2e6f758073
Iman <iman.ison@gmail.com>
2018-09-18 23:49:11 +1000

    Collect children of Desktop

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -3,6 +3,13 @@ import './Desktop.css';
 
 class Desktop extends Component {
     
+    constructor(props) {
+        super(props)
+        this.state = {childrenExtraAttributes: {TEST:"test"}}
+
+        //this.mapChildren.bind(this)
+        console.log("Desktop is created")
+    }
     componentDidMount() {
 
     }
@@ -12,35 +19,52 @@ class Desktop extends Component {
     }
 
     onDropHandler(e) {
-        console.log("DROP: " + e)
+        console.log("DROP: " + e.dataTransfer.getData('text'))
     }
 
     render() {
         
-        let dragableChildren = React.Children.map(this.props.children, (child)=>{
-            if (['Shortcut', 'Window'].indexOf(child.type.name) === -1){
-                return child
-            }
-            return React.cloneElement(child, {
-                attributes:{
-                    draggable: true, 
-                    onDragStart: (e) => {
-                        console.log("DRAG START: " + e.target.id)
-                        //e.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
-                        // TODO: investigate what is the best data to transfer
-                        e.dataTransfer.setData('text/plain', e.target.id)
+        let dragableChildren = React.Children.map(this.props.children, (child, index)=>{
+                let childKey = child.key
+                if (childKey){
+                    if (this.state.childrenExtraAttributes.hasOwnProperty(childKey)) {
+                        let oldKey = childKey
+                        while (this.state.childrenExtraAttributes.hasOwnProperty(childKey = oldKey + "_" + index)) {++index}
+                        console.log('WARNING: Doplicate key has been found in Desktop children and one of them is renamed from "'
+                        + oldKey + '" to "' + childKey)
                     }
-            }})
-        })
+                }else {
+                    while (this.state.childrenExtraAttributes.hasOwnProperty(childKey = child.type.name + "_" + index)) {++index}
+                }
+        
+                if (!this.state.childrenExtraAttributes.hasOwnProperty(childKey))
+                    // eslint-disable-next-line
+                    this.state.childrenExtraAttributes[childKey] = {}
+        
+                if (['Shortcut', 'Window'].indexOf(child.type.name) > -1){
+                    // eslint-disable-next-line
+                    this.state.childrenExtraAttributes[childKey] = {
+                        draggable: true, 
+                        onDragStart: (e) => {
+                            console.log("DRAG START: " + e.target.id)
+                            //e.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
+                            // TODO: investigate what is the best data to transfer
+                            e.dataTransfer.setData('text/plain', e.target.id)
+                        }}
+                }
+        
+                return React.cloneElement(child, { key: childKey, attributes: this.state.childrenExtraAttributes[childKey]})
+            })
 
         return (
+
             <div className="desktop" 
                 onDragOver={this.onDragOverHandler}
                 onDrop={this.onDropHandler}
             >
                 {dragableChildren}
             </div>
-        );
+        )
     }
 }
 
