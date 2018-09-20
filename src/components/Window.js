9f0bb64f93ae7d9d077ea3aaaa973cb825484fe0
Iman <iman.ison@gmail.com>
2018-09-20 17:07:57 +1000

    styling window

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './window.css';
 
 class Window extends Component {
     render() {
         return (
             <div className="window" {...this.props.attributes}>
-                
+            <div className="header">
+                <span className="title">{this.props.title}</span>
+                <button className="close"/>
+                <button className="maximize"/>
+                <button className="minimize"/>
+            </div>
+            <div className="content">
+                {this.props.children}
+            </div>
             </div>
         );
     }
